de21c88f821332989cf723bba5f65cdfa8ecfd00
TobeyMarshallC <jackluosi@outlook.com>
2017-08-17 23:26:38 +0800

    字符串操作类

--- src/org/jacktools/str/StringUtils.java
+++ src/org/jacktools/str/StringUtils.java
@@ -1,7 +1,28 @@
 package org.jacktools.str;
 
+import java.lang.Object;
+
 /**
- * Created by Administrator on 2017/8/17.
+ * 字符串工具类
+ * Created by jackluosi on 2017/8/17.
  */
 public class StringUtils {
+
+    /**
+     * 判断是否为空
+     * @param object 传入需要判断的对象
+     * @return true表示为空，false表示不为空
+     */
+    public static boolean isNullOrEmpty(Object object){
+        if(object == null){
+            return true;
+        }
+        if(object.toString() == null){
+            return true;
+        }
+        if("".equals(object.toString()) || 0 == object.toString().length()){
+            return true;
+        }
+        return false;
+    }
 }
